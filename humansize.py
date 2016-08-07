a4fde9c47709b46d905a78d1c157783954697386
Yuzhi Liu <liuyuzhi83@gmail.com>
2016-08-07 01:48:26 -0400

    correct the bug in humansize.py
    add conda.txt as suggested by Jesse

--- humansize.py
+++ humansize.py
@@ -28,7 +28,7 @@ def approximate_size(size, a_kilobyte_is_1024_bytes=True):
     for suffix in SUFFIXES[multiple]:
         size /= multiple
         if size < multiple:
-            return '{0:.5f} {4}'.format(size, suffix)
+            return '{0:.1f} {1}'.format(size, suffix)
 
     raise ValueError('number too large')
 
