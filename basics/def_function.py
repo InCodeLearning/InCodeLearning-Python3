9344069d43d19705140d608a4a790bcb13bd921e
Zexi Jesse Zhuang <zhzexi@gmail.com>
2016-08-21 08:44:49 -0700

    default function parameter

--- basics/def_function.py
+++ basics/def_function.py
@@ -1,11 +1,23 @@
 # implicitly return None
 def is_positive(num):
     return True if num > 0 else False
+
+
 print(is_positive(0))
+
+
 # python is dynamically typed, no need to specify type for parameter
 
 
 def is_positive2(num):
     if num > 0:
         return True
+
+
 print(is_positive2(0))
+
+
+def default_param(num=1):
+    return num + 1
+
+print(default_param())
