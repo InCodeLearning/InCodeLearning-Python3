3e059b88f5f11f48b379c00f561c9e73b8b75af5
ZijuanZhang <zijuanzhang@gmail.com>
2016-08-28 15:46:11 -0500

    fit to pep8 requirements

--- data_types/boolean.py
+++ data_types/boolean.py
@@ -1,7 +1,7 @@
-''' Case is sensitive in python. so there are two booleans: True and False
+""" Case is sensitive in python. so there are two booleans: True and False
 It can be assigned as boolean value,, also can be writen as expressions.
 None, False, 0, 0.0, 0j, empty sequence or mapping, such as '', (), [], {}
-'''
+"""
 a = 0
 print(1, bool(a))
 b = None
@@ -17,11 +17,12 @@ print(6, bool(f))
 g = True
 print(7, bool(g))
 
-'''comparison operators: <, >, <=, >=,  =, !=,
-Unlike C, all comparison operations in Python have the same priority, which is lower than that of any arithmetic,
+"""comparison operators: <, >, <=, >=,  =, !=,
+Unlike C, all comparison operations in Python have the same priority,
+which is lower than that of any arithmetic,
 shifting or bitwise operation.
 when you are not sure about the priority, you can use parentheses ().
-'''
+"""
 A = 3
 B = 6
 C = 32
@@ -31,14 +32,16 @@ print(9, B != 8)
 print(10, A > B)
 print(11, A + B < C)
 
-""" related functions (the return value is boolean), such as, is, is not, in, not in etc.
+""" related functions (the return value is boolean),
+such as, is, is not, in, not in etc.
 built-in funcitons: isinstance, issubclass
-string methods, isslnum, isalpha, isdecimal, isdigit, islower, isupper, isspace, istitle......, start with is***. 
+string methods, isslnum, isalpha, isdecimal, isdigit, islower,
+isupper, isspace, istitle......, start with is***.
 """
 D = 'abcdef'
 print(12, 'a' in D)
 print(13, 'h' not in D)
-print(14, '' in D)
+print(14, '' in D)  # empty string returns True all the time.
 print(15, 'abcdef' is D)
 print(16, 'abcd' is not D)
 
@@ -53,38 +56,36 @@ print(19, not A > B)
 print(20, not(A > B or A < C))
 
 
-''' "rich comparison"  special methods
-object.__lt__(self, other)  __le, __eq__, __ne__, __gt__, __ge__,
-
-'''
+# "rich comparison"  special methods object.__lt__(first, second)
+#   __le__, __eq__, __ne__, __gt__, __ge__,
 
 
-def __eq__(self, other):
-    return not self < other and not other < self
+def __eq__(first, second):
+    return not first < second and not second < first
 
 
-def __ne__(self, other):
-    return self < other or other < self     # return not self == other
+def __ne__(first, second):
+    return first < second or second < first     # return not first == other
 
 
-def __lt__(self, other):
-    return self < other
+def __lt__(first, second):
+    return first < second
 
 
-def __le__(self, other):
-    return not self > other
+def __le__(first, second):
+    return not first > second
 
 
-def __gt__(self, other):
-    return other < self
+def __gt__(first, second):
+    return second < first
 
 
-def __ge__(self, other):
-    return not self < other
+def __ge__(first, second):
+    return not first < second
 
 p1 = 3
 p2 = 4
-# print(21, p1.__lt__(p2))
+#  print(21, p1.__lt__(p2))
 print(22, __lt__(p1, p2))
 print(23, __le__(p1, p2))
 print(24, __eq__(p1, p2))
